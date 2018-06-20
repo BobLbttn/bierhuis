2e2f4f81117b9d6508ed3bde79dd095066029225
bob.lamsens <bob.lamsens@OntCur-0E18035F.vdabheverlee.intra>
2018-06-20 10:05:05 +0200

    Added brouwer with corresponding adres

---
+++ src/main/java/be/vdab/entities/Brouwers.java
@@ -0,0 +1,94 @@
+package be.vdab.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.Valid;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.SafeHtml;
+
+import be.vdab.valueobjects.Adres;
+
+@Entity
+@Table(name="brouwers")
+@XmlRootElement
+public class Brouwers implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long brouwernr;
+	@NotBlank
+	@Length(min = 1, max = 50)
+	@SafeHtml
+	private String brnaam;
+	@Valid
+	private Adres adres;
+	private BigDecimal omzet;
+
+	public Brouwers () {}
+	
+	public Brouwers(long brouwernr, String brnaam, Adres adres, BigDecimal omzet) {
+		this.brouwernr = brouwernr;
+		this.brnaam = brnaam;
+		this.adres = adres;
+		this.omzet = omzet;
+	}
+
+	public long getBrouwernr() {
+		return brouwernr;
+	}
+
+	public String getBrnaam() {
+		return brnaam;
+	}
+
+	public Adres getAdres() {
+		return adres;
+	}
+
+	public BigDecimal getOmzet() {
+		return omzet;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adres == null) ? 0 : adres.hashCode());
+		result = prime * result + ((brnaam == null) ? 0 : brnaam.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Brouwers other = (Brouwers) obj;
+		if (adres == null) {
+			if (other.adres != null)
+				return false;
+		} else if (!adres.equals(other.adres))
+			return false;
+		if (brnaam == null) {
+			if (other.brnaam != null)
+				return false;
+		} else if (!brnaam.equals(other.brnaam))
+			return false;
+		return true;
+	}
+
+
+}
