685bddb7ae5cd8c5b73ff9e4643a951df4e30d8b
bob.lamsens <bob.lamsens@OntCur-0E18035F.vdabheverlee.intra>
2018-06-25 11:08:02 +0200

    Bestelbon entity added, change bier entity because we need brouwer to be known in bier object

--- src/main/java/be/vdab/entities/Bier.java
+++ src/main/java/be/vdab/entities/Bier.java
@@ -1,6 +1,7 @@
 package be.vdab.entities;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,11 +11,14 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.SafeHtml;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
 
 @Entity
 @Table(name="bieren")
@@ -31,6 +35,76 @@ public class Bier implements Serializable {
 	@ManyToOne(fetch = FetchType.LAZY, optional=false)
 	@JoinColumn(name = "brouwerid")
 	@NotNull
-	private Brouwer brouwer;	
+	private Brouwer brouwer;
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "soortid")
+	@NotNull
+	private Soort soort;
+	@NotNull
+	@NumberFormat(style = Style.NUMBER)
+	@Min(0)
+	private BigDecimal alcohol;
+	@NotNull
+	@NumberFormat(style = Style.NUMBER)
+	@Min(0)
+	private BigDecimal prijs;
 
+
+	Bier() {}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getNaam() {
+		return naam;
+	}
+
+	public Brouwer getBrouwer() {
+		return brouwer;
+	}
+
+	public Soort getSoort() {
+		return soort;
+	}
+
+	public BigDecimal getAlcohol() {
+		return alcohol;
+	}
+
+	public BigDecimal getPrijs() {
+		return prijs;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((alcohol == null) ? 0 : alcohol.hashCode());
+		result = prime * result + ((naam == null) ? 0 : naam.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bier other = (Bier) obj;
+		if (alcohol == null) {
+			if (other.alcohol != null)
+				return false;
+		} else if (!alcohol.equals(other.alcohol))
+			return false;
+		if (naam == null) {
+			if (other.naam != null)
+				return false;
+		} else if (!naam.equals(other.naam))
+			return false;
+		return true;
+	}
+	
 }
---
+++ src/main/java/be/vdab/valueobjects/BestelbonLijn.java
@@ -0,0 +1,84 @@
+package be.vdab.valueobjects;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
+
+import be.vdab.entities.Bier;
+
+@Embeddable
+public class BestelbonLijn implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "bierid")
+	@NotNull
+	private Bier bier;
+	@NotNull
+	@NumberFormat(style = Style.NUMBER)
+	@Min(1)
+	private Integer aantal;
+	@NotNull
+	@NumberFormat(style = Style.NUMBER)
+	@Min(0)
+	private BigDecimal prijs;
+	
+	BestelbonLijn () {}
+
+	public BestelbonLijn(Bier bier, Integer aantal, BigDecimal prijs) {
+		this.bier = bier;
+		this.aantal = aantal;
+		this.prijs = prijs;
+	}
+
+	public Bier getBier() {
+		return bier;
+	}
+
+	public Integer getAantal() {
+		return aantal;
+	}
+
+	public BigDecimal getPrijs() {
+		return prijs;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bier == null) ? 0 : bier.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BestelbonLijn other = (BestelbonLijn) obj;
+		if (bier == null) {
+			if (other.bier != null)
+				return false;
+		} else if (!bier.equals(other.bier))
+			return false;
+		return true;
+	}
+
+	public BigDecimal totaalBestelbonLijn(){
+		return prijs.multiply(BigDecimal.valueOf(aantal));
+	}
+	
+}
